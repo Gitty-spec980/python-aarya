465fb30ae6e4187ee6de0382d9e8ed16bb6f7b58
Gitty-spec980 <allaraji@gmail.com>
2026-01-06 20:16:06 -0500

    intial commit

---
+++ operators.py
@@ -0,0 +1,25 @@
+rollNo=90
+marks=205.87
+name="Suraper"
+is_student= True
+
+
+
+
+print("Hello, my name is", name)
+print("my roll number is", rollNo)
+
+
+print(type(marks))
+
+
+
+
+s="Coding is fun"
+print(len(s))
+print(s[5])
+print(s[10:13])
+fname="Aaradhya"
+lname="Daggumati"
+
+print("My name is ", fname+" "+lname)
