ca03cf27c80cd6169d7b0718732f4bdc6b05dd43
Gitty-spec980 <allaraji@gmail.com>
2026-01-19 10:20:08 -0500

    initial commit

---
+++ conditions afterclass.py
@@ -0,0 +1,14 @@
+
+name="Dino, and I am a puppy."
+is_student= False
+
+print("Hello, my name is", name)
+
+s="Coding is fun"
+print(len(s))
+print(s[5])
+print(s[10:13])
+fname="Dino"
+lname="and I am a puppy"
+
+print("My name is ", fname+" "+lname)
