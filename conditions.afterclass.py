440baf0a80033b19e6bff21617454fcd987c0608
Gitty-spec980 <allaraji@gmail.com>
2026-01-19 10:27:18 -0500

    initial commit

---
+++ conditions.afterclass.py
@@ -0,0 +1,9 @@
+num=int(input("Enter a number:"))
+if num>0:
+     print("Number positive")
+elif num<0:
+     print("Number is negative")
+elif num==0:
+     print("Number is neutral")
+else:
+     print("Invalid")
