4fbfa1bc07951a6b1ca37c3271758d269f824372
Gitty-spec980 <allaraji@gmail.com>
2026-02-03 20:18:49 -0500

    initial commit

---
+++ tuple.py
@@ -0,0 +1,38 @@
+# Write a program to create a tuple and perform the following operations on that dictionary
+
+# empty tuple
+
+tup1=()
+# tuple having integers
+tup1=(1,2,3,4,5,6,7,8,9)
+# tuple with mixed data types
+tup1=("Deepali Madam,", 12345.678910, True)
+print(tup1)
+
+tup1=("Anjali,", (1,2,3,4,5,6,7,8,9,10))
+# acessing the tuple element
+print(tup1[1][6])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
