1722a4208010cdcce376348688c2e2e5f6c388cd
Gitty-spec980 <allaraji@gmail.com>
2026-03-02 16:35:35 -0500

    initial commit

--- turtleafterclass.py
+++ turtleafterclass.py
@@ -1,8 +1,8 @@
 import turtle
 screen = turtle.Screen()
 t = turtle.Turtle()
-for _ in range():
-   t.forward(100)
-   t.right(144) 
+for _ in range(4):
+   t.forward(90)
+   t.right(90) 
 
 turtle.done()
