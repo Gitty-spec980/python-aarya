ca01be5e193d90a0a64b86b61f06c0d271b6ab7f
Gitty-spec980 <allaraji@gmail.com>
2026-04-14 20:44:51 -0400

    initial commit

--- practice.py
+++ practice.py
@@ -1,8 +1,8 @@
 # #Python Programs to add 2 numbers Take user input
   
-# #num1= int(input("Enter the first value."))
-# #num2= int(input("Enter the second value"))
-# #print("The answer to this equation is", num1+num2)
+# num1= int(input("Enter the first value."))
+# num2= int(input("Enter the second value"))
+# print("The answer to this equation is", num1+num2)
 
 # l= int(input("Enter the length="))
 # w= int(input("Enter the width="))
@@ -12,19 +12,41 @@
 
 
 
-#Write a program to ask the user for their age and print:"Child" if age < 13"Teenager" if 13 ≤ age < 20"Adult" if 20 ≤ age < 60"Senior" if age ≥ 60
+# #Write a program to ask the user for their age and print:"Child" if age < 13"Teenager" if 13 ≤ age < 20"Adult" if 20 ≤ age < 60"Senior" if age ≥ 60
 
-age= int(input("Enter your age in this box: "))
-if age<13:
-    print("You are considered a child in modern day society.")
-elif age>=13 and age<20:
-    print("You are considered a tennager or adolecent in modern day society.")
-elif age>=20 and age<60:
-    print("You are considered an adult in modern day society.")
-elif age>=60:
-    print("You are considered a senior or elderly or millenial or stone age in modern day society.")
-else:
-    print("Invalid input. ENter a valid one next time.")
+# age= int(input("Enter your age in this box: "))
+# if age<13:
+#     print("You are considered a child in modern day society.")
+# elif age>=13 and age<20:
+#     print("You are considered a tennager or adolecent in modern day society.")
+# elif age>=20 and age<60:
+#     print("You are considered an adult in modern day society.")
+# elif age>=60:
+#     print("You are considered a senior or elderly or millenial or stone age in modern day society.")
+# else:
+#     print("Invalid input. ENter a valid one next time.")
 
+#Write a program for print command
 
+# import keyword
 
+# print("The keywords for Python are \n")
+# print(keyword.kwlist)
+
+#Write a Python program to count the number of even and odd numbers in a series of numbers. 2,6,9,4,1,7,34,29,74,11
+
+even=0
+odd=0
+nums=(2,6,9,4,1,7,34,29,74,11)
+even_num=[]
+odd_num=[]
+for i in nums:
+    if i%2==0:
+        even=even+1
+        even_num.append(i)
+    else:
+        odd=odd+1
+        odd_num.append(i)
+print("The number of odd digits is", odd,"and the number of even digits is", even)
+print(even_num)
+print(odd_num)
