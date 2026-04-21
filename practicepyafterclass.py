124024c92c405fd85f6665a340ece9a719df0e1c
Gitty-spec980 <allaraji@gmail.com>
2026-04-21 19:36:53 -0400

    initial commit

--- practicepyafterclass.py
+++ practicepyafterclass.py
@@ -1,2 +1,6 @@
-bday= str(input("Enter the enter your birthday="))
-print("Your birthday is", bday)
+#day= str(input("Enter the enter your birthday="))
+#print("Your birthday is", bday)
+str1 = 'Congragulations '
+str2 =  'Maya'
+
+print('str1 + str2 =', str1 + str2)
