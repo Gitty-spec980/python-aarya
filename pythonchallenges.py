4c2a33dcb1b5b174927ccbf06fcad4e00f54ebda
Gitty-spec980 <allaraji@gmail.com>
2026-06-23 20:44:07 -0400

    initial commit

---
+++ pythonchallenges.py
@@ -0,0 +1,64 @@
+# # v=4
+# # w=5
+# # x=8
+# # y=2
+# # z=0
+# # z= (v+w) * x/y
+# # print("Value of (v+w) * x/y is", z)
+
+
+
+# # name="Ira"
+# # age=14
+
+# # if name == "Ira" or name == "Ronan" and age >=2 :
+# #     print("Hello. Welocme.")
+# # else :
+# #     print("Good Bye.")
+
+# n=int(input("The numerator is "))
+# d=int(input("The denomenator is "))
+
+# if n%d == 0:
+#     print(n ,"Is completly divisible by", d)
+# else:
+#     print("Your values do not go in evenly and will result in a decimal.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+
+sum = mean1*total_number
+print("The sum of 40 number: ",sum)
+
+
+num2=sum-((wrong_number)-(correct_number))
+print("sum-((wromg_number))-((correct_number)): ",num2)
+
+
+mean2=num2/total_number
+print(mean2)
