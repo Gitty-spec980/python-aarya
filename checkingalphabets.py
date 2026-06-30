0d21608030743a19cf2babd955f72df098be4fbd
Gitty-spec980 <allaraji@gmail.com>
2026-06-30 16:08:39 -0400

    initial commit

---
+++ checkingalphabets.py
@@ -0,0 +1,8 @@
+user_input_char = input("Put a value here to see if it is an alphabet- ")
+
+if user_input_char: True
+print("This value is true")
+
+if user_input_char: False
+print("This value is False")
+
