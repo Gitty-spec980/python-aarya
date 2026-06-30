9146b1a8f605469f159063a490d1f59e80d6cb96
Gitty-spec980 <allaraji@gmail.com>
2026-06-30 12:24:58 -0400

    initial commit

--- pythonoperators1afterclass.py
+++ pythonoperators1afterclass.py
@@ -1,19 +1,3 @@
-def get_positive_number():
-    while True:
-        try:
-            num = float(input("Enter a number: "))
-            if num < 0:
-                print(" Square root for this number is not real. Try again.")
-            else:
-                return num
-        except ValueError:
-            print(" Invalid input. Please enter a numeric value.")
-
-
-if __name__ == "__main__":
-    number = get_positive_number()
-
-    
-    square_root = number ** 0.5
-
-print(" The square root of",number "is",square_root)
+number = 17
+square_root = number ** 0.5
+print("The square root is:", square_root)
