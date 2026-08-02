a465fbc570ef4cc9ce7c0e94f20f6f1b91063c8d
Gitty-spec980 <allaraji@gmail.com>
2026-08-02 11:22:52 -0400

    initial commit'

---
+++ powercalculator.py
@@ -0,0 +1,9 @@
+# base = 2
+# exponent = 3
+# result = base ** exponent 
+# print(result) OR
+
+base=int(input("Enter your base number here- "))
+exponent=int(input("Enter your exponent here- "))
+result= base**exponent
+print(result)
