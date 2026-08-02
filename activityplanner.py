7595f4f38617181af1b4e3871633b2eab9bb2ac6
Gitty-spec980 <allaraji@gmail.com>
2026-08-02 11:00:56 -0400

    initial commit

---
+++ activityplanner.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+
+
+if age >= 15:
+    
+    if age <= 20:
+        print("Your age is between 15 and 20 years.")
+    else:
+        print("Your age is not between 15 and 20 years.")
+else:
+    print("Your age is not between 15 and 20 years.")
