6a469b55b7a6aac7bfe1faecf5faaf266af0095e
Gitty-spec980 <allaraji@gmail.com>
2026-09-21 17:06:55 -0400

    initial commit

---
+++ billanddiscou.py
@@ -0,0 +1,28 @@
+valid = False
+ 
+while not valid:
+    try:
+    
+        bill_amount, discount_percent, people = input(
+            "Enter bill amount, discount percent, and people separated by commas: "
+        ).split(",")
+ 
+        bill_amount = float(bill_amount)
+        discount_percent = float(discount_percent)
+        people = int(people)
+ 
+        if bill_amount <= 0 or discount_percent < 0 or people < 0:
+            raise ValueError
+        
+        discount_amount = bill_amount * discount_percent / 100
+        final_amount = bill_amount - discount_amount
+ 
+        amount_per_person = final_amount / people
+ 
+    except ValueError:
+        print("Invalid.")
+ 
+    except ZeroDivisionError:
+        print("People cannot be 0. Please enter at least 1 person.")
+ 
+    
